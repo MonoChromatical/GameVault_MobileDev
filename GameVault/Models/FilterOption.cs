8dcbe94fbfe40979c647c2397328ffe59434a425
Cameron Hendrikse <20152311@tafe.wa.edu.au>
2026-04-22 17:36:00 +0800

    add a simple game model

---
+++ GameVault/Models/FilterOption.cs
@@ -0,0 +1,8 @@
+﻿namespace GameVault.Models
+{
+    public class FilterOption
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+}
---
+++ GameVault/Models/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace GameVault.Models
+{
+    public class Game
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Platform { get; set; } = string.Empty;
+    }
+}
