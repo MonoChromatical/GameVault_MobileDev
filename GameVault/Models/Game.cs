c97834954a97c5b0f593ed2e13255723273732a7
Cameron Hendrikse <20152311@tafe.wa.edu.au>
2026-04-23 19:28:00 +0800

    add more game info

--- GameVault/Models/Game.cs
+++ GameVault/Models/Game.cs
@@ -2,8 +2,12 @@
 {
     public class Game
     {
+        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Genre { get; set; } = string.Empty;
         public string Platform { get; set; } = string.Empty;
+        public string ReleaseDate { get; set; } = string.Empty;
+        public double Rating { get; set; }
+        public string Description { get; set; } = string.Empty;
     }
 }
