e90e965558087fd1c72b38d4c34af9513feb892c
Cameron Hendrikse <20152311@tafe.wa.edu.au>
2026-06-12 14:38:37 +0800

    Continues Dev #3 - Go to game card from saved games in library

--- GameVault/Views/LibraryPage.xaml.cs
+++ GameVault/Views/LibraryPage.xaml.cs
@@ -2,14 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using GameVault.ViewModels;
+using GameVault.Models;
+using GameVault.Services;
 
 namespace GameVault.Views
 {
     public partial class LibraryPage : ContentPage
     {
         private readonly LibraryViewModel viewModel;
+        private readonly SelectedGameService selectedGameService;
 
-        public LibraryPage(LibraryViewModel viewModel)
+        public LibraryPage(LibraryViewModel viewModel, SelectedGameService selectedGameService)
         {
             InitializeComponent();
 
@@ -18,6 +21,7 @@ namespace GameVault.Views
             // BindingContext connects LibraryPage.xaml labels/lists to LibraryViewModel.
             this.viewModel = viewModel;
             BindingContext = viewModel;
+            this.selectedGameService = selectedGameService;
         }
 
         protected override async void OnAppearing()
@@ -27,6 +31,22 @@ namespace GameVault.Views
             // FLOW:
             // Every time the Library page opens, reload saved games from SQLite.
             await viewModel.LoadLibraryAsync();
+        }
+
+        private async void GameCard_Tapped(object? sender, TappedEventArgs e)
+        {
+            if (sender is not Border border)
+            {
+                return;
+            }
+
+            if (border.BindingContext is not SavedGame selectedGame)
+            {
+                return;
+            }
+
+            selectedGameService.SelectedGame = selectedGame;
+            await Shell.Current.GoToAsync(nameof(GameDetailsPage));
         }
     }
 }
